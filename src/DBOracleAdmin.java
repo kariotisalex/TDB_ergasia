e4367100bbf8391eede11d1086d243241635fafc
alexa <alexa@DESKTOP-GPBF7TJ>
2020-11-15 14:12:57 +0200

    new commit

---
+++ src/DBOracleAdmin.java
@@ -0,0 +1,92 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alexa
+ */
+public class DBOracleAdmin {
+    
+    private static String     OdriverClassName = "oracle.jdbc.OracleDriver" ;
+    private static String     Ourl = "jdbc:oracle:thin:@192.168.6.21:1521:dblabs" ;
+    private static Connection OdbConnection = null;
+    private static String     Ousername = "";
+    private static String     Opasswd = "";
+    private static Statement  Ostatement = null;
+    private static ResultSet  Ors = null;
+
+    public DBOracleAdmin(String Ousername,String Opasswd) {
+        this.Ousername = Ousername;
+        this.Opasswd = Opasswd;
+        
+        
+    }
+
+    Statement connectToDB(){
+        try {
+            Class.forName (OdriverClassName);
+            OdbConnection = DriverManager.getConnection (Ourl, Ousername, Opasswd);
+            Ostatement    = OdbConnection.createStatement();
+            
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("Den egine sindesi");
+        }
+             
+        return Ostatement;
+    }
+    
+    Statement getStatement(){
+        return Ostatement;
+    }
+    
+    void closeit(){
+        try {
+            OdbConnection.close();
+        } catch (Exception e) {
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/DBPostresqlAdmin.java
@@ -0,0 +1,75 @@
+
+import java.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alexa
+ */
+public class DBPostresqlAdmin {
+    private static String PdriverClassName = "org.postgresql.Driver";
+    private static String Purl = "jdbc:postgresql://dblabs.it.teithe.gr:5432/";
+    private static Connection PdbConnection = null;
+    private static String Pusername = "";
+    private static String Ppasswd = "";
+    private static Statement Pstatement = null;
+    private static ResultSet Prs = null;
+
+    public DBPostresqlAdmin(String Pusername, String Ppasswd) {
+        this.Pusername = Pusername;
+        this.Ppasswd = Ppasswd;
+        Purl = "jdbc:postgresql://dblabs.it.teithe.gr:5432/"+ Pusername;
+    }
+    
+     Statement connectToDB(){
+        try {
+            Class.forName (PdriverClassName);
+            PdbConnection = DriverManager.getConnection (Purl, Pusername, Ppasswd);
+            Pstatement    = PdbConnection.createStatement();
+            
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("Den egine sindesi");
+        }
+             
+        return Pstatement;
+    }
+    
+    Statement getStatement(){
+        return Pstatement;
+    }
+    
+    
+        void closeit(){
+        try {
+            PdbConnection.close();
+        } catch (Exception e) {
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
